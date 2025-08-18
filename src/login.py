208314dcc2ad06df8cdc1a9db7c80f330e965e58
breno portella <bp.bohry@gmail.com>
2025-08-18 13:14:13 +0100

    update login

--- src/login.py
+++ src/login.py
@@ -7,16 +7,17 @@ from src.defines import WAIT_TIME
 
 
 def login(driver, nif, password):
+    nif = str(nif).replace(".0", "").strip()
     driver.get(
         'https://faturas.portaldasfinancas.gov.pt/painelEmitente.action'
     )
     aut_nif = WebDriverWait(driver, WAIT_TIME).until(
-        EC.presence_of_all_elements_located((By.XPATH, '/html/body/div[1]/section/div[1]/div/div/label[2]'))
+        EC.presence_of_all_elements_located((By.XPATH, '/html/body/div/div/main/div[2]/div[2]/div[1]/div[1]/div/div/button[2]/div'))
     )
     aut_nif[0].click()
 
     field_nif = WebDriverWait(driver, WAIT_TIME).until(
-        EC.presence_of_all_elements_located((By.ID, 'username'))
+        EC.presence_of_all_elements_located((By.NAME, 'username'))
     )
     field_nif[0].send_keys(nif)
 
@@ -26,9 +27,10 @@ def login(driver, nif, password):
     field_password[0].send_keys(password)
     
     bt_login = WebDriverWait(driver, WAIT_TIME).until(
-        EC.presence_of_all_elements_located((By.ID, 'sbmtLogin'))
+        EC.presence_of_all_elements_located((By.XPATH, '/html/body/div/div/main/div[2]/div[2]/div[1]/div[3]/form/button'))
     )
     bt_login[0].click()
+    return True
 
 wrong_expired_time = 2
 def wrong_password(driver):
